d24b330e6677c888f48b076d626b33261b3db456
trevor <me@trevorbehnke.com>
2021-08-08 16:13:29 -0700

    first commit

---
+++ src/Die.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./Die.css";
+
+class Die extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <i className={`Die fas fa-dice-${this.props.face}`}></i>;
+  }
+}
+
+export default Die;
